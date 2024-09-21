e95757f75b7a8d8ed7af99318e71a7a63c010248
Shivam_175 <120707485+fake-engg@users.noreply.github.com>
2024-09-21 22:44:02 +0530

    Create Prime number within a range.py

---
+++ Python Coding/Prime number within a range.py
@@ -0,0 +1,11 @@
+lower = int(input("Enter lower limit: "))
+upper = int(input("Enter upper limit: "))
+
+for num in range(lower,upper+1):
+  # 1 is not a prime number
+    if num>1:
+        for i in range(2,num):
+            if num%i ==0:
+                break
+        else:
+            print(num)
